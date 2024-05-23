15dcf4827552b85fa75cdb1d97c75b6a3f52cf2a
stautuan <stautuan89@gmail.com>
2024-05-23 03:52:26 -0400

    Changed file paths for better organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import AppLayout from './components/AppLayout/AppLayout';
-import Home from './components/Home/Home';
-import About from './components/About/About';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
 import Apod from './pages/Apod/Apod';
 import Curiosity from './pages/Curiosity/Curiosity';
 
